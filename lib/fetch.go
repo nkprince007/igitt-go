7c3513be32a36c25bb01435bbc13630272d93235
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-05-09 11:59:31 +0530

    fetch.go: Add Get, Post, Delete methods

--- lib/fetch.go
+++ lib/fetch.go
@@ -1,12 +1,14 @@
 package lib
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
-// ResponseError indicates an error while fetching a HTTP response
+// ResponseError indicates an error while fetching a HTTP response.
 type ResponseError struct {
 	code int
 	s    string
@@ -16,25 +18,80 @@ func (e *ResponseError) Error() string {
 	return fmt.Sprintf("ResponseError: Response('%d', '%s')", e.code, e.s)
 }
 
-var client = &http.Client{}
+var client = &http.Client{
+	Timeout: time.Second * 5,
+}
 
-// Get retrieves data from URL
-func Get(token string, url string) ([]byte, error) {
-	req, err := http.NewRequest("GET", url, nil)
+func prepareRequest(token string, url string, method string, data []byte) (
+	*http.Request, error) {
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
 	if err != nil {
 		return nil, err
 	}
 	if token != "" {
-		req.Header.Add("Authorization", "token: "+token)
+		req.Header.Add("Authorization", "token "+token)
 	}
+	req.Header.Add("Content-Type", "application/json")
+	return req, nil
+}
+
+func sendRequest(req *http.Request, data chan<- []byte, e chan<- error) {
+	defer close(e)
+	defer close(data)
+
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		e <- err
+		data <- nil
+		return
 	}
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
-	if resp.StatusCode >= 300 && resp.StatusCode != 304 {
-		return nil, &ResponseError{resp.StatusCode, string(body)}
+	if err != nil {
+		e <- err
+		data <- nil
+		return
 	}
-	return body, err
+
+	if resp.StatusCode >= 300 && resp.StatusCode != 304 {
+		e <- &ResponseError{resp.StatusCode, string(body)}
+		data <- nil
+		return
+	}
+
+	e <- nil
+	data <- body
+}
+
+// Get retrieves data from URL.
+func Get(token string, url string, data chan<- []byte, e chan<- error) {
+	req, err := prepareRequest(token, url, "GET", nil)
+	if err != nil {
+		e <- err
+		data <- nil
+	}
+
+	go sendRequest(req, data, e)
+}
+
+// Post sends data to URL.
+func Post(token string, url string, data chan []byte, e chan<- error) {
+	req, err := prepareRequest(token, url, "POST", <-data)
+	if err != nil {
+		e <- err
+		data <- nil
+	}
+
+	go sendRequest(req, data, e)
+}
+
+// Delete removes data from URL.
+func Delete(token string, url string, data chan []byte, e chan<- error) {
+	req, err := prepareRequest(token, url, "DELETE", <-data)
+	if err != nil {
+		e <- err
+		data <- nil
+	}
+	go sendRequest(req, data, e)
 }
--- lib/fetch_test.go
+++ lib/fetch_test.go
@@ -6,27 +6,28 @@ import (
 
 	vcr "github.com/ad2games/vcr-go"
 	"github.com/nkprince007/igitt-go/lib"
+	"github.com/stretchr/testify/assert"
 )
 
-type response struct {
-	UserId    int  `json:"userId"`
-	Id        int  `json:"id"`
-	Completed bool `json:"completed"`
-}
-
 func TestGet(t *testing.T) {
+	type response struct {
+		UserId    int  `json:"userId"`
+		Id        int  `json:"id"`
+		Completed bool `json:"completed"`
+	}
+
 	vcr.Start("get", nil)
 	defer vcr.Stop()
 
 	var got response
 
-	body, err := lib.Get("bleh", "https://jsonplaceholder.typicode.com/todos/1")
-	if err != nil {
+	data, e := make(chan []byte), make(chan error)
+	go lib.Get("bleh", "https://jsonplaceholder.typicode.com/todos/1", data, e)
+	if err := <-e; err != nil {
 		t.Error(err)
 	}
 
-	err = json.Unmarshal(body, &got)
-	if err != nil {
+	if err := json.Unmarshal(<-data, &got); err != nil {
 		t.Error(err)
 	}
 
@@ -34,4 +35,50 @@ func TestGet(t *testing.T) {
 	if got != want {
 		t.Errorf("Get failed, got: '%v', want: '%v'", got, want)
 	}
+}
+
+func TestPost(t *testing.T) {
+	type response struct {
+		UserId string `json:"id"`
+		Job    string `json:"job"`
+		Name   string `json:"name"`
+	}
+
+	vcr.Start("post", nil)
+	defer vcr.Stop()
+
+	data, e := make(chan []byte), make(chan error)
+	go lib.Post("", "https://reqres.in/api/users", data, e)
+	data <- []byte(`{"name": "morpheus", "job": "leader"}`)
+
+	if err := <-e; err != nil {
+		t.Error(err)
+	}
+
+	var got response
+	body := <-data
+	if err := json.Unmarshal(body, &got); err != nil {
+		t.Error(err)
+	}
+
+	want := response{"753", "leader", "morpheus"}
+	if got != want {
+		t.Errorf("Post failed, got: '%v', want: '%v'", got, want)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	vcr.Start("delete", nil)
+	defer vcr.Stop()
+
+	data, e := make(chan []byte), make(chan error)
+	go lib.Delete("", "https://reqres.in/api/users/2", data, e)
+	data <- nil
+
+	if err := <-e; err != nil {
+		t.Error(err)
+	}
+
+	got, want := <-data, make([]byte, 0)
+	assert.Equal(t, want, got)
 }
